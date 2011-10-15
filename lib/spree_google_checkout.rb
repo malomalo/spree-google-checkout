72723c4a0fc650d647457a41680241e3c1dcf5ad
Jon Bracy <jonbracy@gmail.com>
2011-10-15 15:10:32 -0500

    trying to get gc working on spree 0.70

--- lib/spree_google_checkout.rb
+++ lib/spree_google_checkout.rb
@@ -4,15 +4,18 @@ require 'spree_core'
 
 module SpreeGoogleCheckout
   class Engine < Rails::Engine
-
+    engine_name 'spree_google_checkout'
+    
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
-      # register the BillingIntegration
-      Billing::GoogleCheckout.register
+    end
+    
+    initializer "spree_google_checkout.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Billing::GoogleCheckout
     end
 
     config.to_prepare &method(:activate).to_proc
