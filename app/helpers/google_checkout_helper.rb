edb70879f471d32e2586f7e5e9d614f2f10452f4
Jon Bracy <jonbracy@gmail.com>
2012-02-12 13:41:27 -0800

    split helpers out

--- app/helpers/google_checkout_helper.rb
+++ app/helpers/google_checkout_helper.rb
@@ -1,6 +1,13 @@
 module GoogleCheckoutHelper
 
   def google_checkout_button(merchant_id)    
+    image_submit_tag(google_checkout_image_url, 
+        :name => "Google Checkout", 
+        :alt => "Fast checkout through Google",
+        :height => 46, :width => 180 )
+  end
+
+  def google_checkout_image_url()
     img_src = Billing::GoogleCheckout.current[:use_sandbox] ?
       "http://sandbox.google.com/checkout/buttons/checkout.gif" :
       "https://checkout.google.com/buttons/checkout.gif"
@@ -8,12 +15,11 @@ module GoogleCheckoutHelper
               :w => 180, :h => 46, :style => "white",
               :variant => "text", :loc => "en_US" }
     params_str = params_hash.to_a.inject([]){|arr, p| arr << p.join('=')}.join('&')
-    
-    
-    image_submit_tag( [img_src, params_str].join('?'), 
-        :name => "Google Checkout", 
-        :alt => "Fast checkout through Google",
-        :height => 46, :width => 180 )
+    [img_src, params_str].join('?')
+  end
+
+  def google_checkout_image()
+    image_tag(google_checkout_image_url, :alt => 'Fast Checkout through Google', :height => 46, :width => 180)
   end
 
 end
